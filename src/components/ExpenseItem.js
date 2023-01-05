1fe3c4eb0ed7afc0a69751a76aa055bd46bf0b05
BerndlSA <119590242+BerndlSA@users.noreply.github.com>
2023-01-05 15:07:00 +0000

    Added Buttons

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import plusButton from '../PlusButton.png';
+import minusButton from '../MinusButton.png';
 
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -41,8 +43,8 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        <td><button onClick={event=> increaseAllocation(props.name)}><img style={{ width: 25, height: 25 }} src={plusButton} alt="Increase Allocation"/></button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}><img style={{ width: 25, height: 25 }} src={minusButton} alt="Decrease Allocation"/></button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
