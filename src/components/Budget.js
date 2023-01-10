0240e1c37de52c7cee478b23b7b6c7d5c6860ab0
Alphadragonx <blokesh585@gmail.com>
2023-01-10 07:26:41 -0500

    practice assignment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,15 @@
 
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+        </div>
+    );
+};
+
+export default Budget;
