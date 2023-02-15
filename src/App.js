7062e62422f386e3ac31c47dbf85d131467c52b0
Amulyaaaaaa <123067234+Amulyaaaaaaa@users.noreply.github.com>
2023-02-15 17:24:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        {/* Add Budget component here under */}   
+                // Budget component
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
 
                         {/* Add Remaining component here under */}        
 
