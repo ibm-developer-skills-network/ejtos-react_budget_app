3d2e327985c6818be002ae84496229a20a2c47c0
deadclown09 <mesandesh.2014@gmail.com>
2023-03-04 18:04:04 +0545

    Created the components and made them functional

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+//importing components
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AllocationForm from "./components/AllocationForm";
 
-//Code to import Budget.js
-import Budget from './components/Budget';
+//importing contexts
+import { AppProvider } from "./context/AppContext";
 
-// Add code to import the other components here under
-
-
-import { AppProvider } from './context/AppContext';
 const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
-
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
-                </div>
-            </div>
-        </AppProvider>
-    );
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">Company's Budget Allocation</h1>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <Budget />
+          </div>
+          <div className="col-sm">
+            <Remaining />
+          </div>
+          <div className="col-sm">
+            <ExpenseTotal />
+          </div>
+          <h1 className="mt-3">Allocation</h1>
+          <div>
+            <ExpenseList />
+          </div>
+          <h1 className="mt-3">Change Allocation</h1>
+          <div>
+            <AllocationForm />
+          </div>
+        </div>
+      </div>
+    </AppProvider>
+  );
 };
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,43 @@
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { TiPlus } from "react-icons/ti";
+import { TiDelete } from "react-icons/ti";
 
+const ExpenseItem = (props) => {
+  const { dispatch } = useContext(AppContext);
+
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: props.id,
+    });
+  };
+
+  const increaseAllocation = (name) => {
+    const expense = {
+      name,
+      cost: 10,
+    };
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.cost}</td>
+      <td>
+        <TiPlus
+          size="1.25em"
+          onClick={(event) => increaseAllocation(props.name)}
+        />
+      </td>
+      <td>
+        <TiDelete size="1.25em" onClick={handleDeleteExpense} />
+      </td>
+    </tr>
+  );
+};
+
+export default ExpenseItem;
