e43e65f70e5963e7c1c6a4fa0af696d7f673349f
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-03-18 15:27:48 -0400

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,53 @@
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
+
+const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+    
+    return (
+    <tr>
+        <td>{props.name}</td>
+        <td>£{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)} style={{color: 'white', size: '1em', backgroundColor:'green', borderWidth: 0, borderRadius:'50%'}}>+</button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)} style={{color: 'white', size: '1em', backgroundColor:'red', borderWidth: 0, borderRadius:'50%'}}>-</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+    </tr>
+    );
+};
+
+export default ExpenseItem;
 
