fbdd12d2cd821cf90776551616fb890a4a7d5c3e
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-03-18 15:26:32 -0400

    Update Budget.js
    current

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,47 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const { dispatch,remaining  } = useContext(AppContext);
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+    const [action, setAction] = useState('');
+
+    const submitEvent = () => {
+
+            if(cost > remaining) {
+                alert("The value cannot exceed remaining funds  £"+remaining);
+                setCost("");
+                return;
+            }
+
+        const expense = {
+            name: name,
+            cost: parseInt(cost),
+        };
+        if(action === "Reduce") {
+            dispatch({
+                type: 'RED_EXPENSE',
+                payload: expense,
+            });
+        } else {
+                dispatch({
+                    type: 'ADD_EXPENSE',
+                    payload: expense,
+                });
+            }
+    };
+
+    return (
+        <div className='alert alert-secondary'>
+                <form action="/action_page.php"></form>
+                <label for="quantity">Budget: £ </label>{
+                    <><input type="number" id="quantity" min="0" max="20000">
+                   </input> </>
+                }
+        </div>
+    );
+};
+
+export default Budget;
 
