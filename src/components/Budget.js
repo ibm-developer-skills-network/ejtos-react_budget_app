3edfcec466cb8dba90722cd6a0d940119d8644fb
jsavich683 <jsavich683@gmail.com>
2023-03-22 15:08:29 -0400

    changes made from theia lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,30 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+	const { budget,dispatch,expenses } = useContext(AppContext);
+
+	const changeBudget = (val)=>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+
+		
+		if(val<totalExpenses) {
+			alert("You cannot reduce the budget that is already allocated!");
+		} else {
+			dispatch({
+				type: 'SET_BUDGET',
+				payload: val,
+			})
+			}
+	}
+	
+	return (
+		<div className='alert alert-secondary'>
+			<input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
+		</div>
+	);
 };
+
 export default Budget;
