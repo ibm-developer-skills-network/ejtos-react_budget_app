15a4537734369adde2756f7c032df874863cf741
rik-phoenix84 <riccardo.abballe@gmail.com>
2023-03-22 12:28:34 -0400

    modifications in Budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,35 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-const Budget = () => {
-    const { budget } = useContext(AppContext);
+
+
+const Budget = (props) => {
+    const  { budget } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
+
+    
+
+    const changeBudget = () => {
+
+        const budget = 10;
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: budget
+        });
+
+    }
+
     return (
         <div className='alert alert-secondary'>
             <span>Budget: £{budget}</span>
+            <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value={props.budget}
+                        step={10}
+                        style={{ marginLeft: '5rem' , size: 10}}
+                        onChange={(event) => changeBudget(event.target.value)}>
+                        </input>
         </div>
     );
 };
