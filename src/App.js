2bf537528e1c835b92490bbe06d9ddb9a70842ab
Umar Alkali <morepps@yahoo.com>
2023-03-27 09:04:31 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import Allocationform from './components/AllocationForm';
 
 
 import { AppProvider } from './context/AppContext';
@@ -16,17 +21,36 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
-
+                                        // Budget component
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         {/* Add Remaining component here under */}        
-
+                // Remaining component
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         {/* Add ExpenseTotal component here under */}        
-                       
+                          // ExpenseTotal component
+                <div className='col-sm'>
+                       <ExpenseTotal />
+                    </div>
                         {/* Add ExpenseList component here under */}         
-
+                // ExpenseList component
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                         {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
+                // ExpenseItem component
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
+                        {/* Add Allocationform component here under */}        
+                // Allocationform component
+                    <div className='col-sm'>
+                        <Allocationform />
+                    </div>
+                    
                 </div>
             </div>
         </AppProvider>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,21 @@
-
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+        </div>
+    );
+};
+export default Budget;import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+        </div>
+    );
+};
+export default Budget;
