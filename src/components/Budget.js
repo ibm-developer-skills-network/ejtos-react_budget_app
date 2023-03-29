413f977744772cbf766fb69ae3e70569dbb28663
Sav-Roy <126276992+Sav-Roy@users.noreply.github.com>
2023-03-29 14:07:34 -0500

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget, totalExpenses, currency } = useContext(AppContext);
+  const [updatedBudget, setupdatedBudget] = useState(budget);
+  console.log('Expenses' + totalExpenses);
+  const withBudget = (e) => {
+    const newBudget = parseInt(e.target.value);
+    if (newBudget > 20000) {
+      alert("Cannot be greater than 20,000");
+      setupdatedBudget(budget);
+      return;
+    } else if(newBudget < totalExpenses) {
+        alert("Cannot be less than" + totalExpenses);
+        setupdatedBudget(budget);
+        return;
+    }else
+     {
+      setupdatedBudget(newBudget);
+    }
+  }
+  return (
+    <div className='alert alert-secondary'>
+      <span>Budget {currency}:  
+        <input type="number"
+          value={updatedBudget}
+          step={10}
+          onChange={withBudget}/>
+      </span>
+    </div>
+  );
 };
 export default Budget;
