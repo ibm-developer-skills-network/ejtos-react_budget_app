cb36f3fae5cba3fe4eeec886cd2b4f075bb0502a
Isaele Araujo <araujoisaele@gmail.com>
2023-04-03 14:04:53 -0400

    changing final project

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -3,7 +3,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -35,46 +35,39 @@ const AllocationForm = (props) => {
     };
 
     return (
-        <div>
-            <div className='row'>
-
-            <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
-                    <div className="input-group-prepend">
-                <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
-                  </div>
-                  <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
-                        <option defaultValue>Choose...</option>
-                        <option value="Marketing" name="marketing"> Marketing</option>
-                <option value="Sales" name="sales">Sales</option>
-                <option value="Finance" name="finance">Finance</option>
-                <option value="HR" name="hr">HR</option>
-                <option value="IT" name="it">IT</option>
-                <option value="Admin" name="admin">Admin</option>
-                  </select>
-
-                    <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+        <div className="input-group mb-3">
+        <div className="input-group-prepend">
+            <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
+            </div>
+                <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                    <option defaultValue>Choose...</option>
+                    <option value="Marketing" name="Marketing"> Marketing</option>
+                    <option value="Sales" name="Sales">Sales</option>
+                    <option value="Finance" name="Finance">Finance</option>
+                    <option value="Human Resource" name="Human Resource">HR</option>
+                    <option value="IT" name="IT">IT</option>
+                </select>
+            <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
                 <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
-                  </div>
-                  <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
-                        <option defaultValue value="Add" name="Add">Add</option>
-                <option value="Reduce" name="Reduce">Reduce</option>
-                  </select>
-
-                    <input
-                        required='required'
-                        type='number'
-                        id='cost'
-                        value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
-                        onChange={(event) => setCost(event.target.value)}>
-                        </input>
-
-                    <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
-                        Save
-                    </button>
                 </div>
-                </div>
-
+                <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                    <option defaultValue value="Add" name="Add">Add</option>
+                    <option value="Reduce" name="Reduce">Reduce</option>
+                </select>
+            <div className="input-group-prepend" style={{ marginLeft: '2rem', size: 10}}>
+                {currency}
+            </div>
+            <input className="input-group-prepend"
+                required='required'
+                type='number'
+                id='cost'
+                value={cost}
+                style={{ marginLeft: '0.25rem'}}
+                onChange={(event) => setCost(event.target.value)}>
+            </input>
+            <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                Save
+            </button>
         </div>
     );
 };
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -17,22 +18,31 @@ const ExpenseItem = (props) => {
             name: name,
             cost: 10,
         };
-
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
         });
 
     }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name,
+            cost: 10,
+        };
+        dispatch({
+            type: "RED_EXPENSE",
+            payload: expense,
+        });
+    };
 
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{currency}{props.cost}</td>
+            <td><AiFillPlusCircle size="2.25em" color="green" onClick={(event) => increaseAllocation(props.name)}/></td>
+            <td><AiFillMinusCircle size="2.25em" color="red" onClick={(event) => decreaseAllocation(props.name)}/></td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
-
 export default ExpenseItem;
