52985a2b32fd811e8ecbfd0b3e77bb06b510550d
Abhishek25104 <sri.abhishek.25104@gmail.com>
2023-04-04 02:06:44 -0400

    coursera final assignment react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
+
 
 // Add code to import the other components here under
 
@@ -15,17 +21,30 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        { <div className='col-sm'>
+                        <Budget />
+                    </div>}        
 
-                        {/* Add Remaining component here under */}        
+                        { <div className='col-sm'>
+                        <Remaining />
+                    </div>}        
 
-                        {/* Add ExpenseTotal component here under */}        
+                        {                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+}        
                        
-                        {/* Add ExpenseList component here under */}         
+                        {<div className='col-sm'>
+                        <ExpenseList />
+                    </div>}       
 
-                        {/* Add ExpenseItem component here under */}        
+                        {<div className='col-sm'>
+                        <ExpenseItem />
+                    </div>}        
 
-                        {/* Add AllocationForm component here under */}        
+                        {<div className='col-sm'>
+                        <AllocationForm />
+                    </div>}        
 
                 </div>
             </div>
