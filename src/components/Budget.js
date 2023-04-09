f432fe7ec197709fdf4e9dc09bd26181f4702293
muthwasboniso <sibbmut@gmail.com>
2023-04-09 09:00:29 -0400

    Changed to editable budget value

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,19 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useState } from 'react';
+//import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const [ budget, setBudget ] = useState('');
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                value={budget}
+                style={{ marginLeft: '2rem' , size: 10}}
+                onChange={(event) => setBudget(event.target.value)}>
+            </input>
         </div>
     );
 };
