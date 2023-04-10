a73cf128767e81957f2bcc1786af7a3e3e7e1bd5
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-09 18:39:14 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,7 +4,7 @@ const Budget = () => {
     const { budget } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £{<input type="number" />}</span>
         </div>
     );
 };
