f9bd93c888338748d399b36a25461ec54fd9ee4a
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-10 19:54:17 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
     const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{<input type="number" />}</span>
-        </div>
-    );
-};
+    const remaining = require("./Remaining")
+        if ({ budget } > 20000) {
+            alert("The value cannot exceed remaining funds  £"+({ remaining }));
+            return;
+        } else {
+            return (
+                <div className='alert alert-secondary'>
+                    <span>Budget: £{<input type="number" max="20000" />}</span>
+                </div>
+        );
+    };
+}
 export default Budget;
