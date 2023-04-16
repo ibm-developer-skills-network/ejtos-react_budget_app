af5f2e738c795b4e2d968e518a9627293a23c911
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-16 13:28:38 -0600

    Update Budget (New).js

--- src/components/Budget (New).js
+++ src/components/Budget (New).js
@@ -1,22 +1,28 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-import Remaining from "./Remaining"
 
 const Budget = () => {
-    const { budget, expenses } = useContext(AppContext);
+    const { budget } = useContext(AppContext);
+    const remaining = require("./Remaining")
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £{<input type="number" max="20000" />}</span>
+            if (({ budget } > 20000) {
+                alert("The value cannot exceed remaining funds  £"+({ remaining }));
+            };                
+                return;
+        </div>
+
+const New Budget = () => {    
+    const { budget, Remaining } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         const value = parseInt(event.target.value);
-        if (value > expenses) {
-            alert("The value cannot exceed remaining funds £"+(budget-Remaining));
-        } else {
-            setNewBudget(value);
-        }
-    }
-}
-return (
-Budget: £
-);
-}
+    if (value > expenses) {
+        alert("The value cannot exceed remaining funds £"+(budget-Remaining));
+    } else {
+        setNewBudget(value);
+    };
+};
 
 export default Budget;
