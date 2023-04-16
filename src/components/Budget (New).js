87c8ac4b7533a9c3d5ebad2075ea2eae4cf52225
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-16 13:40:03 -0600

    Update Budget (New).js

--- src/components/Budget (New).js
+++ src/components/Budget (New).js
@@ -8,21 +8,21 @@ const Budget = () => {
         <div className='alert alert-secondary'>
             <span>Budget: £{<input type="number" max="20000" />}</span>
             if (({ budget } > 20000) {
-                alert("The value cannot exceed remaining funds  £"+({ remaining }));
+                alert("The value cannot exceed remaining funds  £"+({ remaining }))};
             };                
                 return;
         </div>
 
-const New Budget = () => {    
-    const { budget, Remaining } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        const value = parseInt(event.target.value);
-    if (value > expenses) {
-        alert alert-secondary("The value cannot exceed remaining funds £"+(budget-Remaining));
-    } else {
+    ); const NewBudget = () => {    
+        const { budget, Remaining } = useContext(AppContext);
+        const [newBudget, setNewBudget] = useState(budget);
+        const handleBudgetChange = (event) => {
+            const value = parseInt(event.target.value);
+        if (value > expenses) {
+            alert("The value cannot exceed remaining funds £"+(budget-Remaining));
+        } else {
         setNewBudget(value);
     };
 };
 
-export default Budget;
+export default Budget
