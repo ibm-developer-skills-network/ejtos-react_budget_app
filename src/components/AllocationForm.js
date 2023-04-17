9d87d2a00b8064cbb09305f8b5995ac7805fa7aa
muthwasboniso <sibbmut@gmail.com>
2023-04-17 04:22:36 -0400

    All project tasks completed

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -58,13 +58,13 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    <span style={{ marginLeft: '2rem', marginTop: '0.3rem' }}>{currency}</span>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{marginLeft: '0.5rem', size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,19 +1,41 @@
-import React, { useState } from 'react';
-//import { AppContext } from '../context/AppContext';
+import React, { useState, useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const [ budget, setBudget ] = useState('');
+    const [ budget, setValue ] = useState(2000);
+    const { expenses, currency } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const setBudget = (event) => {
+        const inputNumber = event.target.value;
+        setValue(inputNumber);
+        if (inputNumber > 20000){
+            alert("The value cannot exceed £20000");
+                setValue(budget);
+                return;
+        }
+        if (inputNumber < totalExpenses){
+            alert("You cannot reduce the budget value lower than the spending!");
+                setValue(budget);
+                return;
+        }
+    }
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £</span>
+            <span>Budget: {currency}</span>
             <input
                 required='required'
                 type='number'
                 id='budget'
                 value={budget}
-                style={{ marginLeft: '2rem' , size: 10}}
-                onChange={(event) => setBudget(event.target.value)}>
+                step={10}
+                style={{width: 150}}
+                onChange={setBudget}>
             </input>
+            
         </div>
     );
 };
