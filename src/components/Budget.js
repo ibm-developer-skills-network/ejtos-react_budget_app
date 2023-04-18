e1f2dd1f1978de274067542735885a678c5d6d2d
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-17 21:56:25 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,15 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
-    const submitEvent () => {
+    const [setBudget] = useState('');
+    const submitEvent = () => {
         if (budget > 20000) {
             alert("The value cannot exceed £20000");
             setBudget({ budget });
             return (
                 <div className='alert alert-secondary'>
-                    <span>Budget: £{<input type="number" />}</span>
+                    <span>Budget: £{budget} {<input type="number" />}</span>
                 </div>
             );
         };
