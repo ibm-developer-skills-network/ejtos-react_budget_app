08c11d9de5f779c81a9986b1fcb8bf336f65812d
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-17 21:57:23 -0600

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,15 +1,29 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext'; 
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import { Budget } from '../components/Budget';
+
 const Remaining = () => {
-    const { expenses, budget } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total = total + item.cost);
-    }, 0);
-    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
-    return (
-        <div className={`alert ${alertType}`}>
-            <span>Remaining: £{budget - totalExpenses}</span>
-        </div>
-    );
+  const { expenses, budget } = useContext(AppContext);
+  const [setBudget] = useState('');
+  const totalExpenses = expenses.reduce((total, item) => {
+    return total + item.cost;
+  }, 0);
+  const remaining = budget - totalExpenses;
+
+  const handleBudgetChange = (event) => {
+    const newBudget = parseFloat(event.target.value);
+    if (Budget < remaining) {
+      alert(`The value cannot exceed remaining funds £{remaining}`);
+    } else {
+      setBudget(newBudget);
+    }
+  };
+
+  return (
+    <div className="alert alert-light">
+      <span>Remaining: £{remaining}</span>
+    </div>
+  );
 };
+
 export default Remaining;
