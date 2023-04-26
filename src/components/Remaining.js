311e8535801d28c18f2ac8596aae954fc8cf4e17
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-25 19:15:36 -0600

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-import { Budget } from '../components/Budget';
+import Budget from '../components/Budget';
 
 const Remaining = () => {
   const { expenses, budget } = useContext(AppContext);
