1b2552d1ad9e50765d601d556cab4fbbf16f5305
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-26 21:34:25 -0600

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,14 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import { Budget } from '../components/Budget';
+
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total += item.cost);
-    }, 0);
-    return (
-        <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+  const { expenses } = useContext(AppContext);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return total + item.cost;
+  }, 0);
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleAddExpense = () => {
+    // check if total expenses exceed budget
+    if (totalExpenses > Budget) {
+      alert('Total expenses exceed the budget.');
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  };
+
+  return (
+    <div>
+      {showAlert && (
+        <div className="alert alert-primary">
+          <span>Spent so far: £{totalExpenses}</span>
         </div>
-    );
+      )}
+      <button onClick={handleAddExpense}>Add expense</button>
+    </div>
+  );
 };
+
 export default ExpenseTotal;
