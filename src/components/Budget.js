e2d1f40a2464aefa9b7cc04a4798a1321953293e
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-27 19:03:04 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, setBudget } = useContext(AppContext);
+    const { budget, setBudget, Currency } = useContext(AppContext);
     const [inputBudget, setInputBudget] = useState('');
 
     const handleInputChange = (e) => {
@@ -23,9 +23,9 @@ const Budget = () => {
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget} </span>
+            <span>Budget: </span>
             <form onSubmit={handleSubmit}>
-                <input type="number" value={inputBudget} onChange={handleInputChange} />
+             £<input type="number" value={inputBudget} onChange={handleInputChange} step="10" />
                 <button type="submit">Update Budget</button>
             </form>
         </div>
