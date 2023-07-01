c89bc07312e93827ce43a5eb20389e10bd1f442f
JPigeon252 <rsannareddy@gmail.com>
2023-06-30 23:32:51 -0400

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 // Add code to import the other components here under
 
@@ -15,20 +19,29 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
-
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
-                </div>
-            </div>
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>      
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
+                    </div>
+                        <h3 className='mt-3'>Allocation</h3>
+                            <div className='row '>
+                                <div className='col-sm'>
+                                    <ExpenseList />
+                                </div>
+                            </div>
+                        <h3 className='mt-3'>Change allocation</h3>
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                <AllocationForm />
+                            </div>
+                        </div>
+                    </div>
         </AppProvider>
     );
 };
