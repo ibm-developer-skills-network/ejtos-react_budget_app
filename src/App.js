7a505a8f336bb6dc253c57e46bba4b2dfb1bf889
23BThomas <136824128+23BThomas@users.noreply.github.com>
2023-07-05 19:31:26 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { AppProvider } from './context/AppContext';
+
+//Code to import Budget.js
 import Budget from './components/Budget';
+// Add code to import the other components here under
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
@@ -14,22 +17,33 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
+        {/* Add Budget component here under */} 
                     <div className='col-sm'>
                         <Budget />
                     </div>
+         {/* Add Remaining component here under */}
                     <div className='col-sm'>
                         <RemainingBudget />
                     </div>
+    {/* Add ExpenseTotal component here under */}
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
                 </div>
+    {/* Add ExpenseList component here under */}
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
                 </div>
+    {/* Add ExpenseItem component here under */}
+<div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
+                </div>
+{/* Add AllocationForm component here under */}
                 <h3 className='mt-3'>Change allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
