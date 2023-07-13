d38d3caa075cf9f613eb3d5606b506461927a05d
mgschall <michelle.g.schall@disney.com>
2023-07-13 16:10:17 -0400

    Changes to budget for increase descrease
    Signed-off-by: mgschall <michelle.g.schall@disney.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,19 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, setBudget } = useContext(AppContext);
+    
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value={budget}
+                        style={{ marginLeft: '1rem' }}
+                        onChange={(event) => setBudget(event.target.value)}>
+                        </input>           
         </div>
     );
 };
