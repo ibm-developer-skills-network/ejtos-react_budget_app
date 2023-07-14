b8862d2ecfb35a1a90a048cd4587f9842fc99cba
Xinrui6 <100579162+Xinrui6@users.noreply.github.com>
2023-07-14 10:30:47 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,31 +21,34 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
-                            <div className = 'row mt-3'>
+                            <div className = 'col-sm'>
                                 <Budget />
                             </div>
                         {/* Add Remaining component here under */} 
-                            <div className = 'row mt-3'>
+                            <div className = 'col-sm'>
                                 <Remaining />
                             </div>
 
                         {/* Add ExpenseTotal component here under */}        
-                           <div className = 'row mt-3'>
+                           <div className = 'col-sm'>
                                 <ExpenseTotal />
                             </div>
+                        </div>      
+                        <h3 className='mt-3'>Allocation</h3>
+                        <div className = 'row'>
                         {/* Add ExpenseList component here under */}         
-                            <div className = 'row mt-3'>
+                            <div className = 'col-sm'>
                                 <ExpenseList />
                             </div>
                         {/* Add ExpenseItem component here under */}        
-                            <div className = 'row mt-3'>
+                            <div className = 'col-sm'>
                                 <ExpenseItem />
                             </div>
                         {/* Add AllocationForm component here under */}        
-                            <div className = 'row mt-3'>
+                            <div className = 'col-sm'>
                                 <AllocationForm />
                             </div>
-                </div>
+                    </div>
             </div>
         </AppProvider>
     );
