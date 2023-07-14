6420feb28955cf00054e8b1a53802a5eca52a9f2
Xinrui6 <100579162+Xinrui6@users.noreply.github.com>
2023-07-14 10:32:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,6 +44,9 @@ const App = () => {
                             <div className = 'col-sm'>
                                 <ExpenseItem />
                             </div>
+                        </div>
+                        <h3 className='mt-3'>Change allocation</h3>
+                        <div className='row mt-3'>
                         {/* Add AllocationForm component here under */}        
                             <div className = 'col-sm'>
                                 <AllocationForm />
