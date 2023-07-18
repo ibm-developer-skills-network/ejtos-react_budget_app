e19407417f59eb20b3d65ac77326a53b007288ae
Xinrui6 <100579162+Xinrui6@users.noreply.github.com>
2023-07-18 14:06:06 +0800

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget, dispatch, currency } = useContext(AppContext);
