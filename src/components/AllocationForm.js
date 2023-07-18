2f39ad3bba236973a81efaa825584f31072bc123
Xinrui6 <100579162+Xinrui6@users.noreply.github.com>
2023-07-18 13:50:13 +0800

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,13 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency } = useContext(AppContext);
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
     const [action, setAction] = useState('');
     const submitEvent = () => {
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert("The value cannot exceed remaining funds" + currency + remaining);
                 setCost("");
                 return;
             }
@@ -50,7 +50,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-                    <input
+                    {currency}<input
                         required='required'
                         type='number'
                         id='cost'
