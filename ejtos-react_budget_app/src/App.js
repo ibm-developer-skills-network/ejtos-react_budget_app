6af8c40ef7d2c36d0373f4b6a0905e1ff00e43c4
justobioma@outlook.com <justobioma@outlook.com>
2023-07-28 10:36:53 -0400

    commit to repo

---
+++ ejtos-react_budget_app/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppProvider } from './context/AppContext';
+
+//Code to import Budget.js
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
+
+
+// Add code to import the other components here under
+
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                    <div className='row mt-3'>
+                        {/* Add Budget component here under */} 
+                            
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>       
+
+                        {/* Add Remaining component here under */}        
+                        
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
+                        {/* Add ExpenseTotal component here under */}        
+                    
+                             <div className='col-sm'>
+                                <ExpenseTotal />
+                             </div>
+                        {/* Add ExpenseList component here under */}
+                            <div className='col-sm'>
+                                <ExpenseList/>
+                             </div>         
+
+                        {/* Add ExpenseItem component here under */}
+                            <div className='col-sm'>
+                                <ExpenseItem />
+                             </div>        
+
+                        {/* Add AllocationForm component here under */}
+                            <div className='col-sm'>
+                                <AllocationForm />
+                             </div>        
+
+                </div>
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
