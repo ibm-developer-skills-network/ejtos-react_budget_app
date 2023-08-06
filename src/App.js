3a8a3a06b62a315849592ac93cb87c43fce40dae
Caleb Free <calebdfree@gmail.com>
2023-08-05 20:50:55 -0400

    Changes for final Project

--- src/App.js
+++ src/App.js
@@ -9,23 +9,28 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        {<div className='col-sm'> <Budget /> </div> }        
 
-                        {/* Add Remaining component here under */}        
+                        {<div className='col-sm'> <Remaining /> </div>}        
 
-                        {/* Add ExpenseTotal component here under */}        
+                        {<div className='col-sm'> <ExpenseTotal /> </div>}        
                        
-                        {/* Add ExpenseList component here under */}         
+                        {<div className='col-sm'> <ExpenseList /> </div>}         
 
-                        {/* Add ExpenseItem component here under */}        
+                        {<div className='col-sm'> <ExpenseItem /> </div>}        
 
-                        {/* Add AllocationForm component here under */}        
+                        {<div className='col-sm'> <AllocationForm /> </div>}        
 
                 </div>
             </div>
