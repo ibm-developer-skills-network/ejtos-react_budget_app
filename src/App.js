7c9b9e8168e192acc72242c808bbd8a1b1c5536b
Nick Ezekiel <nickezekias@gmail.com>
2023-08-15 08:23:37 -0400

    add feat budget

--- src/App.js
+++ src/App.js
@@ -14,18 +14,21 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                <div className='row mt-3'>
+                    {/* Add Budget component here under */}
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
 
-                        {/* Add Remaining component here under */}        
+                    {/* Add Remaining component here under */}
 
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
+                    {/* Add ExpenseTotal component here under */}
 
-                        {/* Add ExpenseItem component here under */}        
+                    {/* Add ExpenseList component here under */}
 
-                        {/* Add AllocationForm component here under */}        
+                    {/* Add ExpenseItem component here under */}
+
+                    {/* Add AllocationForm component here under */}
 
                 </div>
             </div>
