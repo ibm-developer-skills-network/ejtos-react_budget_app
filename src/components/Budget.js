c78493914356001d9fd141d474794a9f5945e644
jederx <wdpaschetto@gmail.com>
2023-08-16 12:50:23 -0400

    unfinished changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,33 @@
-
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [value, setValue] = useState(budget);
+    const handleChange = (event) => {
+        setValue(parseInt(event.target.value));
+      };
+    
+    const handleIncrease = () => {
+        setValue(budget + 10);
+      };
+    
+    const handleDecrease = () => {
+        setValue(budget - 10);
+      };
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £
+            <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value={value}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={handleChange}
+                        >
+                    </input>
+            </span>
+        </div>
+    );
+};
+export default Budget;
