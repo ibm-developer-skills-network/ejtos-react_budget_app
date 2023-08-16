9030f113e8c78a98dc4bce70969fb52c82ed71bb
SofakiM <sofia.marioli@hotmail.com>
2023-08-15 21:05:23 -0400

    Putting the UI Components in Place to create the Company’s Budget Allocation application
    Signed-off-by: SofakiM <sofia.marioli@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ import Budget from './components/Budget';
 
 // Add code to import the other components here under
 
+//Code to import Remaining.js
+import Remaining from './components/Remaining';
+
+//Code to import from ExpenseTotal.js
+import ExpenseTotal from './components/ExpenseTotal';
+
+//Code to import from ExpenseList.js
+import ExpenseList from './components/ExpenseList';
+
+//Code to import from ExpenseItem.js
+import ExpenseItem from './components/ExpenseItem';
+
+//Code to import from AllocationForm.js
+import AllocationForm from './components/AllocationForm';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -15,18 +29,30 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
-
-                        {/* Add Remaining component here under */}        
-
+                        {/* Add Budget component here under */}      
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                        {/* Add Remaining component here under */}       
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         {/* Add ExpenseTotal component here under */}        
-                       
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                         {/* Add ExpenseList component here under */}         
-
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                         {/* Add ExpenseItem component here under */}        
-
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
                         {/* Add AllocationForm component here under */}        
-
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
