7f38242c875f201b168b9fb411400d569a6f327b
AgusSoria00 <123342761+AgusSoria00@users.noreply.github.com>
2023-08-23 01:26:05 -0300

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,28 +1,38 @@
 import React, { useContext } from 'react';
-import ExpenseItem from './ExpenseItem';
+import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseList = () => {
-    const { expenses } = useContext(AppContext);
-    
+const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
-        <table className='table'>
-              <thead className="thead-light">
-            <tr>
-              <th scope="col">Department</th>
-              <th scope="col">Allocated Budget</th>
-              <th scope="col">Increase by 10</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-            <tbody>
-            {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
-            ))}
-            </tbody>
-        </table>
+        <tr>
+        <td>{props.name}</td>
+        <td>£{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
     );
 };
 
-export default ExpenseList;
-
+export default ExpenseItem;
