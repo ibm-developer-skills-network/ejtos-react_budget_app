2a952090c9707e582ea4bb6f86b63ca1b1ef3283
3nbar <haidarha59178@gmail.com>
2023-08-23 18:45:14 +0200

    Fuanl

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+
+
 
 // Add code to import the other components here under
 
@@ -15,19 +21,48 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        {/* Add Budget component here under */}   
+
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>     
 
                         {/* Add Remaining component here under */}        
 
-                        {/* Add ExpenseTotal component here under */}        
+
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+
+                        {/* Add ExpenseTotal component here under */}      
+
+
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>  
                        
-                        {/* Add ExpenseList component here under */}         
+                        {/* Add ExpenseList component here under */}    
+
+
 
                         {/* Add ExpenseItem component here under */}        
 
                         {/* Add AllocationForm component here under */}        
 
                 </div>
+
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div> 
+                </div>
+
+                  <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div> 
+                </div>
+
             </div>
         </AppProvider>
     );
