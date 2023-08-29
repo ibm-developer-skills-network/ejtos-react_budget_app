2405be9812cbd474c6d418b147a128d6ed724f4e
Adhish <adhish@gmail.com>
2023-08-29 03:22:32 -0400

    for the lab

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 // Add code to import the other components here under
 
 
@@ -15,18 +19,33 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        {/* Add Budget component here under */}
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>      
 
-                        {/* Add Remaining component here under */}        
+                        {/* Add Remaining component here under */}
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>       
 
-                        {/* Add ExpenseTotal component here under */}        
+                        {/* Add ExpenseTotal component here under */}
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>       
                        
                         {/* Add ExpenseList component here under */}         
-
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                         {/* Add ExpenseItem component here under */}        
-
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
                         {/* Add AllocationForm component here under */}        
-
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
