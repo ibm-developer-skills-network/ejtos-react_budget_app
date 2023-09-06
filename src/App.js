30bc6391864331e5f6ed4582337a859878733d8e
MehtaShivam <mehta.shivam95@gmail.com>
2023-09-06 18:56:08 -0400

    Huge changes regarding redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
@@ -16,17 +20,29 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
-
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                        {/* Add ExpenseTotal component here under */}       
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                         {/* Add ExpenseList component here under */}         
-
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                         {/* Add ExpenseItem component here under */}        
-
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
                         {/* Add AllocationForm component here under */}        
-
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
