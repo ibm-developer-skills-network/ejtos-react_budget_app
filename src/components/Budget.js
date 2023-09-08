46705fa5f160d5c5f700ff330049d3b73d146c09
JackSavino <jack_savino@yahoo.com>
2023-09-08 18:04:43 -0400

    Adding updated Budget.js component file

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,44 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    useEffect(() => {
+        setNewBudget(budget);
+    }, [budget]);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        const inputValue = parseInt(event.target.value);
+        
+        if (inputValue > 20000) {
+            alert('Budget cannot exceed £20,000');
+        } else if (inputValue < totalExpenses) {
+            alert('Budget must be less than spending.');
+        } else {
+            setNewBudget(inputValue);
+        }
+    };
+
+    const saveBudget = () => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget,
+        });
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}
+            onBlur={saveBudget}>
+            </input>
+        </div>
     );
 };
+
 export default Budget;
