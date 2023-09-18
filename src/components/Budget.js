4e64eb31b1a7c50c0abe6892b794a6fe8702e92e
Kartik Grover <you@example.comgroverkartik2000@yahoo.com>
2023-09-18 19:12:12 -0400

    made changes to fix budget update which was not working

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,18 +1,20 @@
-
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
+    const { budget, dispatch } = useContext(AppContext);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const newBudgetValue = parseInt(event.target.value, 10);
+        dispatch({ type: 'SET_BUDGET', payload: newBudgetValue });
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
+
 export default Budget;
