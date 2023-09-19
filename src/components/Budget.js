f819c4b4e7c995722fe2fec90e2dda72f39aea22
Kartik Grover <groverkartik2000@yahoo.com>
2023-09-18 23:50:09 -0400

    added alerts

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,18 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget, dispatch } = useContext(AppContext);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleBudgetChange = (event) => {
         const newBudgetValue = parseInt(event.target.value, 10);
-        dispatch({ type: 'SET_BUDGET', payload: newBudgetValue });
+        if (newBudgetValue <= 20000) {
+            dispatch({ type: 'SET_BUDGET', payload: newBudgetValue });
+            setErrorMessage(''); // Clear any previous error message
+        } else {
+            setErrorMessage('The value cannot exceed 20000');
+        }
     }
 
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-            <input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
+        <div>
+            <div className='alert alert-secondary'>
+                <span>Budget: £{budget}</span>
+                <input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
+            </div>
+            {errorMessage && <p className="alert alert-danger">{errorMessage}</p>}
         </div>
     );
 };
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -7,8 +7,11 @@ const Remaining = () => {
         return (total = total + item.cost);
     }, 0);
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+    const alertMessage = totalExpenses > budget ? 'The value cannot exceed remaining funds' : budget-totalExpenses;
+
     return (
         <div className={`alert ${alertType}`}>
+            {alertMessage && <p>{alertMessage}</p>}
             <span>Remaining: £{budget - totalExpenses}</span>
         </div>
     );
