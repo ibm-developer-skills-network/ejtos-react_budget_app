e9e84caa9bb769b30412f4bc5665f51d9a1ae2a6
Kartik Grover <groverkartik2000@yahoo.com>
2023-09-20 00:17:18 -0400

    changes to add currency dropdown

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency'; // Import the Currency component
 
 const App = () => {
     return (
@@ -22,6 +23,9 @@ const App = () => {
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency /> {/* Include the Currency component */}
                     </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,28 +1,30 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, dispatch } = useContext(AppContext);
-    const [errorMessage, setErrorMessage] = useState('');
+    const { budget, dispatch, expenses } = useContext(AppContext);
 
     const handleBudgetChange = (event) => {
         const newBudgetValue = parseInt(event.target.value, 10);
-        if (newBudgetValue <= 20000) {
-            dispatch({ type: 'SET_BUDGET', payload: newBudgetValue });
-            setErrorMessage(''); // Clear any previous error message
+        const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+
+        if (!isNaN(newBudgetValue) && newBudgetValue >= 0 && newBudgetValue <= 20000) {
+            if (newBudgetValue >= totalExpenses) {
+                dispatch({ type: 'SET_BUDGET', payload: newBudgetValue });
+            } else {
+                // Display a browser alert if the budget is less than total expenses
+                window.alert('Budget cannot be lower than spending.');
+            }
         } else {
-            setErrorMessage('The value cannot exceed 20000');
+            window.alert('Budget cannot be more than 20,000.');
         }
-    }
+    };
 
     return (
-        <div>
             <div className='alert alert-secondary'>
                 <span>Budget: £{budget}</span>
                 <input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
             </div>
-            {errorMessage && <p className="alert alert-danger">{errorMessage}</p>}
-        </div>
     );
 };
 
