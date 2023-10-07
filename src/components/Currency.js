2d00d0b6239b68ef853c91e26e61c0b9d285cec2
Aravind-2010 <mr.aravindsrini@gmail.com>
2023-10-07 09:29:13 -0400

    Updating react Budget app after stylizing
    Signed-off-by: Aravind-2010 <mr.aravindsrini@gmail.com>

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
-  const {dispatch } = useContext(AppContext);
+  const {dispatch  } = useContext(AppContext);
 
     const changeCurrency = (val)=>{
             dispatch({
@@ -10,18 +10,21 @@ const Currency = () => {
                 payload: val,
             })
     }
+
     
 
   return (
-        <div className='alert alert-info' style = {{color:'white'}} > Currency { 
-      <select name="currency" id="currency" onChange={event=>changeCurrency(event.target.value)}>
-        <option value="$">$ Dollar</option>
-        <option value="£">£ Pound</option>
-        <option value="€">€ Euro</option>
-        <option value="₹">₹ Ruppee</option>
+        <div style = {{borderRadius: '5px', color:'white', backgroundColor: 'lightgreen', height: '50px', width: '200px'}}  > Currency({ 
+      <select name="currency" id="currency" style = {{color: 'white', borderColor: 'lightgreen',  marginTop: '1rem', backgroundColor: 'lightgreen'}} onChange={event=>changeCurrency(event.target.value)}>
+        <option value="$" style = {{ color: 'white', }}>$ Dollar</option>
+        <option value="£" style = {{color: 'white'}}>£ Pound</option>
+        <option value="€"style = {{color: 'white'}}> € Euro</option>
+        <option value="₹" style = {{color: 'white'}}>₹ Ruppee</option>
       </select>	
-      }	
+      })
     </div>
+
+
     );
 };
 
