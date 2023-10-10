3e4b8c44c70b1ffb4e380550a1a13ed961807b57
31pablo05 <131816279+31pablo05@users.noreply.github.com>
2023-10-10 10:16:27 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,69 @@
 import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { AppProvider } from './context/AppContext';
+//Code to import Budget.js
 import Budget from './components/Budget';
+
+// Add code to import the other components here under
+import Remaining from './components/Remaining';
+
 import ExpenseTotal from './components/ExpenseTotal';
+
+import { AppProvider } from './context/AppContext';
+
 import ExpenseList from './components/ExpenseList';
+
 import AllocationForm from './components/AllocationForm';
-import RemainingBudget from './components/Remaining';
+
+import Currency from './components/Currency.js';
 
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                <div className='row mt-3'>
+                    <div className='row mt-3'>
+
+                        {/* Add Budget component here under */}        
                     <div className='col-sm'>
                         <Budget />
                     </div>
+                        {/* Add Remaining component here under */}
                     <div className='col-sm'>
-                        <RemainingBudget />
-                    </div>
+                        <Remaining />
+                    </div>     
+
+                        {/* Add ExpenseTotal component here under */}
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
-                </div>
-                <h3 className='mt-3'>Allocation</h3>
-                <div className='row '>
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>
+                    </div>        
+
+                    <h3 className='mt-3'>Allocation</h3>
+
+                        {/* Add ExpenseList component here under */}
+                    <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
+                        </div>         
                     </div>
-                </div>
                 <h3 className='mt-3'>Change allocation</h3>
-                <div className='row mt-3'>
+
+                        {/* Add ExpenseItem component here under */}        
+
+                        {/* Add AllocationForm component here under */}
+                    <div className='row mt-3'>
                     <div className='col-sm'>
                         <AllocationForm/>
-                    </div>
+                    </div>        
+
                 </div>
             </div>
         </AppProvider>
     );
 };
-
 export default App;
