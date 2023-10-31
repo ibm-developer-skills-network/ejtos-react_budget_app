dcd82205e3fc8db9463199e2a0b96e15dea12b5b
Thomas Dambreville <thomasdambreville@proton.me>
2023-10-31 12:04:08 -0400

    Added the basic functionality
    Signed-off-by: Thomas Dambreville <thomasdambreville@proton.me>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,29 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, dispatch } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+
+    useEffect(() => {
+        const handleBudget = (e) => {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: e,
+            });
+        };
+        handleBudget(newBudget)
+    }, [newBudget, dispatch])
+
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
