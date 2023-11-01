bea60b96acea23dc661c20e481843dea9ae7b8ba
Gagandeep-Singh65 <gagandeepswaraj65@gmail.com>
2023-11-01 06:46:32 -0400

    initial commit

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,18 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.value);
+    }
+    return (
+        <div className='alert alert-secondary'>
+        {/* remove {budget} from <span>Budget: £{budget}</span> */}
+        <span>Budget: £ {budget}</span>
+        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
+    );
+};
+export default Budget;
