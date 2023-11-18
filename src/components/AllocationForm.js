687a07262bcb916ef0c50353d6b6b9633d39ddba
rlazic <118766710+rlazic@users.noreply.github.com>
2023-11-18 22:03:57 +0100

    Add files via upload

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -58,6 +58,8 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
+
+                  <span style={{ "position":"relative","right":"-20px"}}> {currency} </span>  
 
                     <input
                         required='required'
