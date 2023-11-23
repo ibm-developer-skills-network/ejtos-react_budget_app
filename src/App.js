76a29f6f7d34655e98446f01a01ccd7fdce8ebc0
lmeany <147493623+lmeany@users.noreply.github.com>
2023-11-23 16:42:59 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { AppProvider } from './context/AppContext';
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
-import RemainingBudget from './components/Remaining';
+import { AppProvider } from './context/AppContext';
 
 const App = () => {
     return (
@@ -14,31 +14,36 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
+                    {/* Budget component */}
                     <div className='col-sm'>
                         <Budget />
-                    </div>
+                    </div>      
+                    {/* Remaining component */}
                     <div className='col-sm'>
-                        <RemainingBudget />
-                    </div>
+                        <Remaining />
+                    </div>   
+                    {/* ExpenseTotal component */}
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
-                </div>
+                </div>  
+                {/* Allocation component  */}
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
+                    {/* Expense list component */}
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
                 </div>
+                {/* Allocation form component */}
                 <h3 className='mt-3'>Change allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
                         <AllocationForm/>
                     </div>
-                </div>
+                </div>       
             </div>
         </AppProvider>
     );
 };
-
 export default App;
