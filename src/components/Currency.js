9a866a7426ab9493f34b3a71e3c10098c4ac5460
lmeany <147493623+lmeany@users.noreply.github.com>
2023-11-23 19:58:49 +0400

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,28 +1,28 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const Location = () => {
+const Currency = () => {
   const {dispatch } = useContext(AppContext);
 
     const changeCurrency = (val)=>{
             dispatch({
                 type: 'CHG_CURRENCY',
                 payload: val,
-            })
-    }
+            });
+    };
     
 
   return (
-        <div className='alert alert-secondary'> Location {
-      <select name="Location" id="Location" onChange={event=>changeCurrency(event.target.value)}>
-        <option value="$">Dollar($)</option>
-        <option value="₹">India(₹)</option>
-        <option value="€">Europe(€)</option>
-        <option value="£">Pound(£)</option>
-      </select>	
-      }	
+    <div className='alert alert-secondary'> Currency {
+        <select name="Currency" id="Currency" onChange={event=>changeCurrency(event.target.value)}>
+            <option value="$">Dollar($)</option>
+            <option value="₹">India(₹)</option>
+            <option value="€">Europe(€)</option>
+            <option value="£">Pound(£)</option>
+        </select>	
+    }	
     </div>
     );
 };
 
-export default Location;
+export default Currency;
