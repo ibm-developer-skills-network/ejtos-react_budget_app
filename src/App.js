28e040ff2336064b5887fd5713197393893f7bb3
lmeany <147493623+lmeany@users.noreply.github.com>
2023-11-23 18:33:44 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
+import Currency from './components/Currency';
 import { AppProvider } from './context/AppContext';
 
 const App = () => {
@@ -25,6 +26,10 @@ const App = () => {
                     {/* ExpenseTotal component */}
                     <div className='col-sm'>
                         <ExpenseTotal />
+                    </div>
+                    {/* Currency component */}
+                    <div className='col-sm'>
+                        <Currency />
                     </div>
                 </div>  
                 {/* Allocation component  */}
