570a9c1eff4b8485f92e0ba2867036da82b74adc
Ahmed Raslan <Ahmedxradslan99@gmail.com>
2023-12-14 12:21:29 -0500

    changes made in theia lab environment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining'
+import ExpenseTotal from './components/ExpenseTotal'
+import ExpenseList from './components/ExpenseList'
+import ExpenseItem from './components/ExpenseItem'
+import AllocationForm from './components/AllocationForm'
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -16,30 +20,38 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                            //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                            //ExpenseTotal component
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
                         }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
-                </div>
+                            </div>
+                                <h3 className='mt-3'>Allocation</h3>
+                                <div className='row '>
+                                    <div className='col-sm'>
+                                        <ExpenseList />
+                                    </div>
+                                </div>
+                                <h3 className='mt-3'>Change allocation</h3>
+                                <div className='row mt-3'>
+                                    <div className='col-sm'>
+                                        <AllocationForm/>
+                                    </div>
+                                </div>
             </div>
         </AppProvider>
     );
