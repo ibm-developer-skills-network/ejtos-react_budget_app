a5597c529d7bced2de84f6d34bf95e870e362043
ECast17 <144755147+ECast17@users.noreply.github.com>
2024-02-20 21:03:46 -0500

    Updated App.js
    Added Budget Component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,10 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            /* Budget component */
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
