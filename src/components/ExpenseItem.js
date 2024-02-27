2a7e37a9a09dc6440614b5a7b87791e1df9c5630
facufdez <facufvet@gmail.com>
2024-02-27 15:58:45 -0300

    button color

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import{FaPlusCircle,FaMinusCircle} from 'react-icons/fa'
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -41,8 +42,12 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency} {props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>➕</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>➖</button></td>
+        <td><FaPlusCircle size="1.5em" color="green"
+          onClick={(event) => increaseAllocation(props.name)}
+        ></FaPlusCircle></td>
+        <td><FaMinusCircle size="1.5em" color="red"
+          onClick={(event) => decreaseAllocation(props.name)}
+        ></FaMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
