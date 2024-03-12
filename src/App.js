07efe883d0062fa0efca54f56cb457aab1d94052
maksim-pokhiliy <maksim.pokhiliy@gmail.com>
2024-03-12 09:43:43 +0200

    code style fixes, updated .gitignore

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppProvider } from "./context/AppContext";
 
-//Code to import Budget.js
-import Budget from './components/Budget';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// Add code to import the other components here under
-
-
-import { AppProvider } from './context/AppContext';
 const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
-                </div>
-            </div>
-        </AppProvider>
-    );
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">Company's Budget Allocation</h1>
+        <div className="row mt-3"></div>
+      </div>
+    </AppProvider>
+  );
 };
+
 export default App;
