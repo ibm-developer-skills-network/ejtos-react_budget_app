04db2668def8e1d37a35d20a83fc265c37b9641b
Broonomo <162533478+Broonomo@users.noreply.github.com>
2024-03-24 09:25:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {                // Budget component
+                                        // Budget component
                           <div className='col-sm'>
                            <Budget />
                           </div>
                             /* Add Budget component here */
-                        }        
+                                
 
                         {
                             /* Add Remaining component here*/
