f00b65268bbd81b257b114c3e28052463281d473
staunch-Emmanuel <enwelemmemmanuel@gmail.com>
2024-03-26 10:43:22 -0400

    Updated budget app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
-import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
-
+// Add code to import the components
 import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency';
+
 const App = () => {
     return (
         <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+            <div className='container bg-light'>
+                {/* Add the code here to add the components*/}
+                <div className='text-center bg-danger text-light mt-1'>
+                    <h1>Enwelem Emmanuel Budget Allocation</h1>
+                </div>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <RemainingBudget />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>
+                </div>
+                <h3 className='mt-2 bg-secondary text-light'>Allocation</h3>
+                <div className='row'>
+                    <div className='col-sm bg-light'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-1 bg-secondary text-light'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm bg-light'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,66 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const BUDGET_MAX_VALUE = 2000;
+
+const Budget = () => {
+  const { budget, totalExpenses,currency, dispatch } = useContext(AppContext);
+
+  const onChangeBudgetHandler = (event) => {
+    const enteredValue = Number(event.target.value);
+
+    // check if the entered value is a number
+    if (Number.isNaN(enteredValue)) {
+      alert('Please enter a valid number.');
+      return;
+    }
+
+    // check if the entered value is an integer number
+    if (!Number.isInteger(enteredValue)) {
+      alert('Please enter an integer number.');
+      return;
+    }
+
+    if (enteredValue < totalExpenses) {
+      alert(
+        "The value of the budget can't be lower than the expenses value " +
+          currency +
+          totalExpenses
+      );
+    } else {
+      if (enteredValue > BUDGET_MAX_VALUE) {
+        alert('Please enter a value less that ' + BUDGET_MAX_VALUE);
+        return;
+      }
+      dispatch({
+        type: 'SET_BUDGET',
+        payload: enteredValue,
+      });
+    }
+  };
+
+  return (
+    <div
+      className="alert alert-secondary"
+      style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
+    >
+      <div>
+        <label htmlFor="budget">Budget:&nbsp;</label>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span>{currency}</span>
+        <input
+          required="required"
+          placeholder='enter your budget'
+          type="number"
+          id="budget"
+          value={budget}
+          step="10"
+          onChange={onChangeBudgetHandler}
+        ></input>
+      </div>
+    </div>
+  );
+};
+
+export default Budget;
