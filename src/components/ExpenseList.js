a07947748d7397ae48636fa854beca66b2f8999f
AYB01 <117665312+AYB01@users.noreply.github.com>
2024-03-28 17:47:03 +0000

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,29 +1,94 @@
+// import React, { useContext } from 'react';
+// import { TiDelete } from 'react-icons/ti';
+// import { AppContext } from '../context/AppContext';
 
+// const ExpenseItem = (props) => {
+//     const { dispatch } = useContext(AppContext);
+
+//     const handleDeleteExpense = () => {
+//         dispatch({
+//             type: 'DELETE_EXPENSE',
+//             payload: props.id,
+//         });
+//     };
+
+//     const increaseAllocation = (name) => {
+//         const expense = {
+//             name: name,
+//             cost: 100000,
+//         };
+
+//         dispatch({
+//             type: 'ADD_EXPENSE',
+//             payload: expense
+//         });
+
+//     }
+
+
+//     return (
+//         <tr>
+//         <td>{props.name}</td>
+//         <td>Rp {props.cost}</td>
+//         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+//         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+//         </tr>
+//     );
+// };
+
+// export default ExpenseItem;
 
 import React, { useContext } from 'react';
-import ExpenseItem from './ExpenseItem';
+
+import { FaPlusCircle,FaMinusCircle } from 'react-icons/fa';
+import { TiDelete} from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseList = () => {
-    const { expenses } = useContext(AppContext);
+const ExpenseItem = (props) => {
+    const { currency, dispatch } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
 
     return (
-        <table className='table'>
-              <thead className="thead-light">
-            <tr>
-              <th scope="col">Department</th>
-              <th scope="col">Allocated Budget</th>
-              <th scope="col">Increase by 10</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-            <tbody>
-            {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
-            ))}
-            </tbody>
-        </table>
+        <tr>
+            <td className="col">{props.name}</td>
+            <td className="col">{currency}{props.cost}</td>
+            <td className="col"><FaPlusCircle size='1.2em' color="#4ead5c" onClick={event => increaseAllocation(props.name)}></FaPlusCircle></td>
+            <td className="col"><FaMinusCircle size='1.2em' color="#af1e11" onClick={event => decreaseAllocation(props.name)}></FaMinusCircle></td>
+            <td className="col"><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
     );
 };
 
-export default ExpenseList;
+export default ExpenseItem;
