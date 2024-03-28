023164b026c4c0393fa09f06bf1954237eb8bd91
AYB01 <117665312+AYB01@users.noreply.github.com>
2024-03-28 17:46:44 +0000

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,40 +1,88 @@
-
-
-import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+const Currency = () => {
+  const { currency, dispatch } = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
-    };
+  const [isOpen, setIsOpen] = useState(false);
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
+  const setCurrencyHandler = (currency) => {
+    dispatch({
+      type: 'CHG_CURRENCY',
+      payload: currency,
+    });
+  };
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
+  //  '£' '€' '$' '₹'
 
+  const currencyLabel = ()=> {
+    switch(currency){
+      case '$' :
+        return '$ Dollar'
+      case '£' :
+        return '£ Pound'
+      case '€' :
+        return '€ Euro'
+      case '₹' :
+        return '₹ Ruppee'
+      default:
+        return ''
     }
+  }
 
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+  return (
+    <div id="currency-menu" className="dropdown" style={{ cursor: 'pointer' }}>
+      <button
+        id="currency-menu-button"
+        className="btn dropdown-toggle"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+        style={{ backgroundColor: '#93e399', color: '#fff' }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Currency {'('}{currencyLabel()}{')'}
+      </button>
+      <ul className={`custom-menu dropdown-menu ${isOpen ? 'show' : ''} `}>
+        <li>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => setCurrencyHandler('$')}
+          >
+           $ Dollar
+          </button>
+        </li>
+        <li>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => setCurrencyHandler('£')}
+          >
+           £ Pound
+          </button>
+        </li>
+        <li>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => setCurrencyHandler('€')}
+          >
+           € Euro
+          </button>
+        </li>
+        <li>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => setCurrencyHandler('₹')}
+          >
+           ₹ Ruppee
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
-export default ExpenseItem;
+export default Currency;
