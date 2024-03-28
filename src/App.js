4689c5baa60c19dd18e95f9ff53030609379c0ec
AYB01 <117665312+AYB01@users.noreply.github.com>
2024-03-28 17:49:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+// Add code to import the components
 import { AppProvider } from './context/AppContext';
 import Budget from './components/Budget';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency';
 
 const App = () => {
     return (
         <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
+            <div className='container bg-light'>
+                {/* Add the code here to add the components*/}
+                <div className='text-center'>
+                    <img src='/FD_Header.png' alt='FD_logo' width='50px'/>
+                </div>
+                <div className='text-center bg-danger text-light mt-1'>
+                    <h1>Fay's Dalgona Budget Allocation</h1>
+                </div>
                 <div className='row mt-3'>
                     <div className='col-sm'>
                         <Budget />
@@ -23,16 +30,19 @@ const App = () => {
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
-                </div>
-                <h3 className='mt-3'>Allocation</h3>
-                <div className='row '>
                     <div className='col-sm'>
+                        <Currency />
+                    </div>
+                </div>
+                <h3 className='mt-2 bg-secondary text-light'>Allocation</h3>
+                <div className='row'>
+                    <div className='col-sm bg-light'>
                         <ExpenseList />
                     </div>
                 </div>
-                <h3 className='mt-3'>Change allocation</h3>
+                <h3 className='mt-1 bg-secondary text-light'>Change allocation</h3>
                 <div className='row mt-3'>
-                    <div className='col-sm'>
+                    <div className='col-sm bg-light'>
                         <AllocationForm/>
                     </div>
                 </div>
@@ -40,5 +50,4 @@ const App = () => {
         </AppProvider>
     );
 };
-
 export default App;
